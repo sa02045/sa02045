f49e1173c4d7ac90a3c480f6f17882da4d674baa
devjojo <sa02045@naver.com>
2024-05-06 20:01:25 +0900

    chore: 블로그 파일 제거

--- gatsby-browser.js
+++
@@ -1,2 +0,0 @@
-import './src/css/normalize.css';
-import './src/css/style.css';
--- src/components/Footer.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function Footer() {
-  return <footer className="border-t h-24"></footer>;
-}
